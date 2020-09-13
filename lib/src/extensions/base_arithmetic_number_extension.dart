5476a8e1a8160449319906ddecc31a058e1a4c77
kaanyagci <kaan.yagci@clique.paris>
2020-09-13 12:41:24 +0200

    Default maxDecimalDigits value added to 12

--- lib/src/extensions/base_arithmetic_number_extension.dart
+++ lib/src/extensions/base_arithmetic_number_extension.dart
@@ -1,8 +1,8 @@
 import 'package:dart_base_arithmetic/src/structs/base_number.dart';
 
 extension BaseArithmeticNumberExtension on num {
-  BaseNumber inBase(int base, {int maxDecimalDigits}) {
-    var result = BaseNumber(base: 3, maxDecimal: maxDecimalDigits);
+  BaseNumber inBase(int base, {int maxDecimalDigits = 12}) {
+    var result = BaseNumber(base: base, maxDecimal: maxDecimalDigits);
     result.parse(this);
     return result;
   }
